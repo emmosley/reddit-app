06185e7d8e9eb0fd28fd253f366b30ed5a78a569
Elizabeth <elizabeth.mosley136@gmail.com>
2020-01-20 12:55:12 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: []
 })
 export class AppComponent {
+  constructor(private apiService: ApiService) {}
   title = 'reddit-app';
+  data = this.apiService.getData().subscribe((data) => this.data = { ...data });
 }
